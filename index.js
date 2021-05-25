4827b32209cc1051d3f81a0718a3b1c819ba890b
3Demon <3demon@bk.ru>
2021-05-25 10:43:50 +0600

    userid from args

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 class SipuniApi {
 
     host = 'http://10.1.0.34';
+    userId;
 
-    constructor() {
-
+    constructor(args) {
+        if (!args || !args.call_args || !args.call_args.pbx_user_id)
+            throw 'user id not set';
+        this.userId = args.call_args.pbx_user_id;
     }
 
-    async sendNotifyWebphone(userId, shortNum) {
+    async sendNotifyWebphone(shortNum) {
         let res = await axios.post(this.host + '/ext/sendNotifyWebPhoneExtension', {
             userId: userId,
             shortNum: shortNum,
             data: {}
         });
-        console.log(process.argv);
         return res.data.success;
-
     }
 }
 
