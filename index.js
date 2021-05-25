6ade7d284dd381e624eee7b38cc674ecec1c99c1
3Demon <3demon@bk.ru>
2021-05-25 10:25:27 +0600

    argv test

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const axios=require('axios');
+const axios = require('axios');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 
-class SipuniApi{
-    constructor () {
+class SipuniApi {
+
+    host = 'http://10.1.0.34';
+
+    constructor() {
+
+    }
+
+    async sendNotifyWebphone(userId, shortNum) {
+        let res = await axios.post(this.host + '/ext/sendNotifyWebPhoneExtension', {
+            userId: userId,
+            shortNum: shortNum,
+            data: {}
+        });
+        console.log(process.argv);
+        return res.data.success;
 
     }
 }
