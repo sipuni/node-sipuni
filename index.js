50476773b7a630fb5bf38c79150c2e719ab7a903
3Demon <3demon@bk.ru>
2021-05-27 14:16:55 +0600

    moved token to headers

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ class SipuniApi {
         if (!token)
             throw 'token not set';
         this.token = token;
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
     }
 
     async sendNotifyWebphone(shortNum) {
         let res = await axios.post(this.host + '/ext/sendNotifyWebPhoneExtension', {
-            token: this.token,
             shortNum: shortNum,
-        }, {headers: {}});
+        });
         return !!res.data.success;
     }
 }
